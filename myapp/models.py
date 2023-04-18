bc4063beca9813c7e358a8761e99c93b318d925c
Олег <106114697+NjemTop@users.noreply.github.com>
2023-04-18 14:16:07 +0300

    добавил изменения

--- myapp/models.py
+++ myapp/models.py
@@ -6,13 +6,13 @@ def generate_unique_id():
     return str(uuid.uuid4())
 
 class BMInfoOnClient(models.Model):
-    client_name = models.TextField(verbose_name="Название клиента", db_index=True)
-    contact_status = models.BooleanField(verbose_name="Активность")
-    service = models.TextField(verbose_name="Обслуживание", default=generate_unique_id)
-    technical_information = models.TextField(verbose_name="Тех информация", default=generate_unique_id)
-    integration = models.TextField(verbose_name="Интеграции", default=generate_unique_id)
-    documents = models.TextField(verbose_name="Документы", default=generate_unique_id)
-    notes = models.CharField(verbose_name="Примечания", max_length=255, null=True, blank=True)
+    client_name = models.TextField(verbose_name="Client_name", db_index=True)
+    contact_status = models.BooleanField(verbose_name="Contact_status")
+    service = models.TextField(verbose_name="Service", default=generate_unique_id)
+    technical_information = models.TextField(verbose_name="Technical_information", default=generate_unique_id)
+    integration = models.TextField(verbose_name="Integration", default=generate_unique_id)
+    documents = models.TextField(verbose_name="Documents", default=generate_unique_id)
+    notes = models.CharField(verbose_name="Notes", max_length=255, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "BM Info on Clients"
@@ -22,7 +22,7 @@ class BMInfoOnClient(models.Model):
         return self.client_name
 
 class Favicon(models.Model):
-    file = models.FileField(upload_to='favicons/') # Исправлен путь upload_to
+    file = models.FileField(upload_to='favicons/')
 
     def __str__(self):
         return self.file.name
