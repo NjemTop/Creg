8cd7ee1583b39b27903371fa074ae024525d4d0d
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-08 02:30:31 +0300

    ревью для читабельности

--- api/response_helpers.py
+++ api/response_helpers.py
@@ -7,16 +7,16 @@ def custom_update_response(instance, request, pk_field, obj_name_field, client_n
     obj_name = getattr(instance, obj_name_field)
     client_name = getattr(getattr(instance, client_name_field).client_info, 'client_name')
     obj_id = getattr(instance, pk_field)
-    return Response(
-        {"message": f"Запись '{obj_name}' клиента '{client_name}' с ID '{obj_id}' обновлена"},
-        status=success_status,
-    )
+    response_data = {
+        "message": f"Запись '{obj_name}' клиента '{client_name}' с ID '{obj_id}' обновлена"
+        }
+    return Response(response_data, status=success_status)
 
 def custom_delete_response(instance, instance_id, name_field, client_field):
     instance_name = getattr(instance, name_field)
     client_name = getattr(getattr(instance, client_field).client_info, 'client_name')
 
     response_data = {
-        'message': f"Запись '{instance_name}' клиента '{client_name}' с ID '{instance_id}' удалена"
+        "message": f"Запись '{instance_name}' клиента '{client_name}' с ID '{instance_id}' удалена"
     }
     return Response(response_data, status=status.HTTP_200_OK)
