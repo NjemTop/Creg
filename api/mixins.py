b205c15c70571ec85b0a801a10829d14db9d0b87
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-08 02:39:47 +0300

    добавил описание и комментарии

--- api/mixins.py
+++ api/mixins.py
@@ -4,15 +4,49 @@ from rest_framework import status
 from .response_helpers import custom_update_response, custom_delete_response
 
 class CustomResponseMixin:
+    """
+    Миксин для создания пользовательских ответов при частичном обновлении (PATCH) и удалении (DELETE) объектов модели.
+    """
+
     def partial_update(self, request, *args, **kwargs):
+        """
+        Частичное обновление объекта модели с пользовательским ответом.
+        :param request: Объект HTTP запроса
+        :param args: Дополнительные аргументы
+        :param kwargs: Дополнительные именованные аргументы
+        :return: Response объект с пользовательским сообщением и кодом статуса
+        """
+
+        # Получаем объект, который нужно обновить
         instance = self.get_object()
+
+        # Вызываем стандартное частичное обновление и сохраняем результат в переменную response
         response = super().partial_update(request, *args, **kwargs)
+
+        # Если статус ответа 200, то заменяем ответ на пользовательский
         if response.status_code == 200:
             response = custom_update_response(instance, request, 'id', 'bm_servers_servers_name', 'client_card')
+        
+        # Возвращаем ответ
         return response
 
     def destroy(self, request, *args, **kwargs):
+        """
+        Удаление объекта модели с пользовательским ответом.
+        :param request: Объект HTTP запроса
+        :param args: Дополнительные аргументы
+        :param kwargs: Дополнительные именованные аргументы
+        :return: Response объект с пользовательским сообщением и кодом статуса
+        """
+
+        # Получаем объект, который нужно удалить
         instance = self.get_object()
+
+        # Сохраняем ID объекта перед удалением
         instance_id = instance.id
+
+        # Выполняем стандартное удаление объекта
         self.perform_destroy(instance)
+
+        # Возвращаем пользовательский ответ с сохраненным ID объекта
         return custom_delete_response(instance, instance_id, 'bm_servers_servers_name', 'client_card')
--- api/response_helpers.py
+++ api/response_helpers.py
@@ -4,19 +4,48 @@ from rest_framework.response import Response
 from rest_framework import status
 
 def custom_update_response(instance, request, pk_field, obj_name_field, client_name_field, success_status=status.HTTP_200_OK):
+    """
+    Функция для создания пользовательского ответа после успешного обновления объекта.
+    :param instance: Обновленный объект модели
+    :param request: Объект HTTP запроса
+    :param pk_field: Название поля с первичным ключом (ID) объекта
+    :param obj_name_field: Название поля с именем объекта (например, название сервера)
+    :param client_name_field: Название поля с информацией о клиенте, которому принадлежит объект
+    :param success_status: Код статуса успешного ответа (по умолчанию 200)
+    :return: Response объект с пользовательским сообщением и кодом статуса
+    """
+
+    # Получаем имя объекта, имя клиента и ID объекта
     obj_name = getattr(instance, obj_name_field)
     client_name = getattr(getattr(instance, client_name_field).client_info, 'client_name')
     obj_id = getattr(instance, pk_field)
+
+    # Создаем словарь с данными для ответа
     response_data = {
         "message": f"Запись '{obj_name}' клиента '{client_name}' с ID '{obj_id}' обновлена"
-        }
+    }
+
+    # Возвращаем Response объект с данными и кодом статуса
     return Response(response_data, status=success_status)
 
 def custom_delete_response(instance, instance_id, name_field, client_field):
+    """
+    Функция для создания пользовательского ответа после успешного удаления объекта.
+    :param instance: Удаленный объект модели
+    :param instance_id: ID удаленного объекта
+    :param name_field: Название поля с именем объекта (например, название сервера)
+    :param client_field: Название поля с информацией о клиенте, которому принадлежал объект
+    :return: Response объект с пользовательским сообщением и кодом статуса
+    """
+
+    # Получаем имя объекта и имя клиента
     instance_name = getattr(instance, name_field)
     client_name = getattr(getattr(instance, client_field).client_info, 'client_name')
 
+    # Создаем словарь с данными для ответа
     response_data = {
         "message": f"Запись '{instance_name}' клиента '{client_name}' с ID '{instance_id}' удалена"
     }
+
+    # Возвращаем Response объект с данными и кодом статуса
     return Response(response_data, status=status.HTTP_200_OK)
