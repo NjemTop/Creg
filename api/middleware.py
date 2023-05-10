fc51ad5d0d901b99541aa0af496a63f2a9e3ec34
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-10 22:45:45 +0300

    добавил исключение для одного базовоного эндпоинта

--- api/middleware.py
+++ api/middleware.py
@@ -7,7 +7,9 @@ class AppendSlashWithPOSTMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path.startswith('/api/') and request.method in ['POST', 'PATCH'] and request.path.endswith('/'):
+        is_exception_endpoint = request.path.startswith('/api/clients/') and request.path.count('/') == 4
+
+        if request.path.startswith('/api/') and request.method in ['POST', 'PATCH'] and request.path.endswith('/') and not is_exception_endpoint:
             return HttpResponseBadRequest("URL не должен заканчиваться на слэш при отправке POST/PATCH-запроса. Используйте URL без завершающего слэша.")
         response = self.get_response(request)
         return response
