1a6588afe546ff3ea70985310e260d83978d40bf
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-11 16:22:10 +0300

    вернул обратно фильтрацию, всё ок

--- crag/settings.py
+++ crag/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'main',
     'rest_framework',
     'drf_yasg',
@@ -54,6 +55,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'JSON_INDENT': 4,
 }
