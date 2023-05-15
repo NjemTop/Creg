4ce006871bcfc6d2de39b373e8ba0582627430bc
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-15 13:54:11 +0300

    добавил логирование в Celery от Джанго

--- crag/celery.py
+++ crag/celery.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+import logging
+from celery.signals import after_setup_logger
+from django.conf import settings
 
 # Устанавливаем переменную окружения DJANGO_SETTINGS_MODULE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crag.settings')
@@ -12,3 +15,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматические загрузка задач из файла tasks.py каждого приложения
 app.autodiscover_tasks()
+
+
+@after_setup_logger.connect
+def setup_loggers(logger, *args, **kwargs):
+    logger.handlers = []
+    logging.config.dictConfig(settings.LOGGING)
