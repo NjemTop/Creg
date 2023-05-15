107f1e980b7d1747b26c2f12d84fc18b9a1b4ba8
Олег <106114697+NjemTop@users.noreply.github.com>
2023-05-15 19:25:17 +0300

    добавил celerybeat как отдельный сервис

--- crag/settings.py
+++ crag/settings.py
@@ -177,6 +177,8 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
 # Настройка, которая убирает слэш в конце "/"
 # APPEND_SLASH = False
 
