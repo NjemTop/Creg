f6ee1e0578f0f4f570eb12b49b0c94ae5ec86a83
Олег <106114697+NjemTop@users.noreply.github.com>
2023-12-24 19:50:58 +0300

    старые скрипты порелаз

--- scripts/test_rmq.py
+++
@@ -1,9 +0,0 @@
-import pika
-
-parameters = pika.ConnectionParameters(host='rabbitmq', port=5672)
-try:
-    connection = pika.BlockingConnection(parameters)
-    print('RabbitMQ is reachable')
-    connection.close()
-except pika.exceptions.AMQPConnectionError:
-    print('RabbitMQ is not reachable')
