439cb88579da6ead32dceec1ad3b7b1ee5e7c121
Олег <106114697+NjemTop@users.noreply.github.com>
2025-06-08 06:57:09 +0300

    убрал всё, оставил только шаблоны

--- backend/apps/mailings/services/base/helpers.py
+++ backend/apps/mailings/services/base/helpers.py
@@ -1,11 +1,1 @@
-def log_updates(logger, message, updates, language):
-    log_message = f"{message} на {language}:\n"
-    for module in updates:
-        if ':' in module:
-            log_message += f"  - {module.split(':')[0]}:\n"
-            for update in module.split(':')[1].split('.'):
-                if update.strip():
-                    log_message += f"      - {update.strip()}.\n"
-        else:
-            log_message += f"  - {module}.\n"
-    logger.info(log_message.strip())
+import os
--- backend/apps/mailings/services/runners/send_test_email.py
+++ backend/apps/mailings/services/runners/send_test_email.py
@@ -1,17 +1,1 @@
 from apps.mailings.services.base.email_sender import EmailSender
-from apps.mailings.services.utils.attachments import clear_attachments_directory
-
-
-def send_test_email(emails, mailing_type, release_type,
-                    server_version=None, ipad_version=None, android_version=None, language='ru'):
-    clear_attachments_directory()
-    sender = EmailSender(
-        emails=emails,
-        mailing_type=mailing_type,
-        release_type=release_type,
-        server_version=server_version,
-        ipad_version=ipad_version,
-        android_version=android_version,
-        language=language
-    )
-    return sender.send_email()
